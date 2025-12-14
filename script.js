e639cfa4eec62c4f7e182837de3ea958e18f81d6
adixmll <adinih757@gmail.com>
2025-12-14 12:59:05 +0700

    Refactor copyPin function and add number display elements
    Refactor copyPin function to accept label and number parameters. Add new elements for number display.

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 let bgm=document.getElementById("bgm");
 let alertBox=document.getElementById("alert");
-let qris=document.getElementById("qrisImg");
+let numberBox=document.getElementById("numberBox");
+let numberLabel=document.getElementById("numberLabel");
+let numberValue=document.getElementById("numberValue");
+let qrisImg=document.getElementById("qrisImg");
+let downloadBtn=document.getElementById("downloadQris");
 let musicOn=true;
 
 function startMusic(){
 if(bgm.paused) bgm.play();
 }
 
-function copyPin(e,n){
+function copyPin(e,label,number){
 e.stopPropagation();
-navigator.clipboard.writeText(n);
+navigator.clipboard.writeText(number);
+
+numberBox.style.display="block";
+numberLabel.innerText=label+" NUMBER";
+numberValue.value=number;
+
+qrisImg.style.display="none";
+downloadBtn.style.display="none";
+
 alertBox.style.display="block";
-qris.style.display="none";
 setTimeout(()=>alertBox.style.display="none",2000);
 }
 
 function showQris(e){
 e.stopPropagation();
-qris.style.display="block";
+numberBox.style.display="none";
+
+qrisImg.style.display="block";
+downloadBtn.style.display="block";
+
 alertBox.style.display="none";
 }
 
